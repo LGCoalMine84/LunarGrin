a44511980f8daed8d13aa99d91cf5f9fc61cdbe0
Chris <chris.mraovich@lunargrin.com>
2014-09-14 23:30:56 -0400

    Added files needed for saving game data

---
+++ ProjectFreedom/Assets/Source/Utils/FileUtils.cs
@@ -0,0 +1,83 @@
+﻿using Logging;
+using System;
+using System.IO;
+
+/// <summary>
+/// File utility class for easing use of 
+/// common file operations.
+/// </summary>
+public class FileUtils
+{
+	#if LOGGING
+	private static ILogger Log = LogFactory.CreateLogger( typeof( FileUtils ) );
+	#endif
+
+	/// <summary>
+	/// Reads data from a file to a string.
+	/// </summary>
+	/// <returns>The string data read from the file.</returns>
+	/// <<param name="filePath">The path of the file to be created.</param>
+	public static String ReadFileToString( String filePath )
+	{
+		#if LOGGING
+		Log.Trace( "Begin String ReadFileToString( String filePath )" );
+		#endif
+
+		#if PARAM_CHECKING
+		if( filePath == null )
+		{
+			throw new ArgumentException( "parameter filePath is required" );
+		}
+		
+		if( String.IsNullOrEmpty( filePath ) )
+		{
+			throw new ArgumentException( "parameter filePath must be non-empty" );
+		}
+		#endif
+		
+		String data = System.IO.File.ReadAllText( filePath );
+
+		#if LOGGING
+		Log.Trace( "End String ReadFileToString( String filePath )" );
+		#endif
+
+		return data;
+	}
+
+	/// <summary>
+	/// Writes string data to a file.
+	/// </summary>
+	/// <param name="filePath">The path of the file to be created.</param>
+	/// <param name="data">The string data to be written to the file.</param>
+	public static void WriteStringToFile( String filePath, String data )
+	{
+		#if LOGGING
+		Log.Trace( "Begin void WriteStringToFile( String filePath, String data )" );
+		#endif
+
+		#if PARAM_CHECKING
+		if( filePath == null )
+		{
+			throw new ArgumentException( "parameter filePath is required" );
+		}
+
+		if( String.IsNullOrEmpty( filePath ) )
+		{
+			throw new ArgumentException( "parameter filePath must be non-empty" );
+		}
+
+		if( data == null )
+		{
+			throw new ArgumentException( "parameter data is required" );
+		}
+		#endif
+
+		StreamWriter sw = File.CreateText( filePath );
+		sw.Write( data );
+		sw.Close();
+
+		#if LOGGING
+		Log.Trace( "End void WriteStringToFile( String filePath, String data )" );
+		#endif
+	}
+}
---
+++ ProjectFreedom/Assets/Source/Utils/JsonSerialization.cs
@@ -0,0 +1,144 @@
+﻿using Logging;
+
+using Pathfinding.Serialization.JsonFx;
+
+using System;
+using System.IO;
+using System.Text;
+
+/// <summary>
+/// This class is the primary API used to deserialize/serialize a data type 
+/// to and from a String of JSON formatted data.
+/// 
+/// In order for a new type to be serialized/deserialized, a converter
+/// must be written for that type and registered with this class.  The
+/// converter must extend the JsonConverter class declared by the JSONFX
+/// 3rd party library.
+/// </summary>
+public class JsonSerialization
+{
+	#if LOGGING
+	private ILogger Log = LogFactory.CreateLogger( typeof( JsonSerialization ) );
+	#endif
+
+	private JsonReaderSettings jsonReaderSettings;
+	private JsonReader jsonReader;
+
+	private JsonWriterSettings jsonWriterSettings;
+	private JsonWriter jsonWriter;
+	private StringBuilder writtenJson;
+
+	/// <summary>
+	/// Initializes a new instance of the <see cref="JsonSerialization"/> class.
+	/// </summary>
+	/// <param name="prettyPrint">If set to <c>true</c>, created Json is formatted/indented.</param>
+	public JsonSerialization( Boolean prettyPrint = false )
+	{
+		#if LOGGING
+		Log.Trace( "Begin JsonSerialization( Boolean prettyPrint = " + prettyPrint + " )" );
+		#endif
+
+		//reader handles Json to object instance conversions
+		jsonReaderSettings = new JsonReaderSettings();
+
+		jsonWriterSettings = new JsonWriterSettings();
+		jsonWriterSettings.PrettyPrint = prettyPrint;
+
+		//writer handles instance to Json conversions
+		writtenJson = new StringBuilder();
+		StringWriter stringWriter = new StringWriter( writtenJson );
+		jsonWriter = new JsonWriter( stringWriter, jsonWriterSettings );
+
+		#if LOGGING
+		Log.Trace( "End JsonSerialization( Boolean prettyPrint = " + prettyPrint + " )" );
+		#endif
+	}
+
+	/// <summary>
+	/// Registers a converter with the JsonSerialization instance.
+	/// </summary>
+	/// <param name="converter">A converter for a particular type, as required by the JSONFX API.</param>
+	public void RegisterConverter( JsonConverter converter )
+	{
+		#if LOGGING
+		Log.Trace( "Begin void RegisterConverter( JsonConverter converter )" );
+		#endif
+
+		#if PARAM_CHECKING
+		if( converter == null )
+		{
+			throw new ArgumentException( "parameter converter is required" );
+		}
+		#endif
+
+		jsonWriterSettings.AddTypeConverter( converter );
+		jsonReaderSettings.AddTypeConverter( converter );
+
+		#if LOGGING
+		Log.Trace( "End void RegisterConverter( JsonConverter converter )" );
+		#endif
+	}
+
+	/// <summary>
+	/// Converts a instance into a String of Json data.
+	/// </summary>
+	/// <returns>A String of Json data containing data that was contained in the instance.</returns>
+	/// <param name="instance">The object instance to be converted into Json.</param>
+	public String ToJson( Object instance )
+	{
+		#if LOGGING
+		Log.Trace( "Begin String ToJson( Object instance )" );
+		#endif
+
+		#if PARAM_CHECKING
+		if( instance == null )
+		{
+			throw new ArgumentException( "parameter instance is required" );
+		}
+		#endif
+
+		jsonWriter.Write( instance );
+		String json = writtenJson.ToString();
+		writtenJson.Length = 0;
+
+		#if LOGGING
+		Log.Trace( "End String ToJson( Object instance )" );
+		#endif
+
+		return json;
+	}
+
+	/// <summary>
+	/// Converts Json data into an instance.
+	/// </summary>
+	/// <returns>The instantiated instance populated with data from the Json String.</returns>
+	/// <param name="type">The type of the instance that should be returned.</param>
+	/// <param name="json">The Json data that is to be converted into an instance.</param>
+	public Object FromJson( Type type, String json )
+	{
+		#if LOGGING
+		Log.Trace( "Begin Object FromJson( Type type, String json )" );
+		#endif
+
+		#if PARAM_CHECKING
+		if( type == null )
+		{
+			throw new ArgumentException( "parameter type is required" );
+		}
+
+		if( json == null )
+		{
+			throw new ArgumentException( "parameter json is required" );
+		}
+		#endif
+
+		JsonReader jsonReader = new JsonReader( json, jsonReaderSettings );
+		Object instance = jsonReader.Deserialize( type );
+
+		#if LOGGING
+		Log.Trace( "End Object FromJson( Type type, String json )" );
+		#endif
+
+		return instance;
+	}
+}
