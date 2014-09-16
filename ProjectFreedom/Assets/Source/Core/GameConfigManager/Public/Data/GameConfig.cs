de47d3c586be4e23cada786f47f0201e3aa39877
johnwhitsell <johnw.whitsell@lunargrin.com>
2014-09-15 20:00:40 -0400

    Putting in initial hooks for saving and loading GameConfig data.

---
+++ ProjectFreedom/Assets/Source/Core/GameConfigManager/Public/Data/GameConfig.cs
@@ -0,0 +1,21 @@
+﻿#region File Header
+// File Name:		GameConfig.cs
+// Author:			John Whitsell
+// Creation Date:	
+//
+// Copyrights:		Copyright 2014
+//					Lunar Grin, LLC.
+//					All rights reserved.
+#endregion
+
+#region Using Directives
+using UnityEngine;
+using System;
+#endregion
+
+public class GameConfig
+{
+	public SoundSettings soundSettings = null;
+	
+	//public VideoSettings videoSettings = null;
+}
